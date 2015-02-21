d9afe59c4d531886e1f69df6942bd20f02b9ade4
Michael Naumov <mnaoumov@gmail.com>
2015-02-21 23:43:32 +0200

    Add script to generate filter-branch

--- MyScript.ps1
+++ MyScript.ps1
@@ -1,14 +1,36 @@
-#requires -version 2.0
+@'
+#!/bin/sh
+n=$GIT_AUTHOR_NAME
+m=$GIT_AUTHOR_EMAIL
 
-$script:ErrorActionPreference = "Stop"
+case ${GIT_AUTHOR_NAME} in
+'@ | Out-File -FilePath MyScript.sh -Encoding Ascii
 
-$someComplexCondition = $false
+Get-Content -Path ".git\svn\authors.txt" | `
+    Select-String -Pattern "^(?<Alias>[\w._]+) = (?<Name>[\w _]+) \<(?<Email>.*)\>" | `
+    ForEach-Object -Process { $_.Matches } | `
+    ForEach-Object -Process `
+        {
+            New-Object -TypeName PSObject -Property `
+                @{
+                    Alias = $_.Groups["Alias"].Value;
+                    Name = $_.Groups["Name"].Value;
+                    Email = $_.Groups["Email"].Value;
+                }
+        } |
+    ForEach-Object -Process `
+        {
+@"
+        "$($_.Alias)") n="$($_.Name)" ; m="$($_.Email)" ;;
+"@
+        } | Add-Content -Path MyScript.sh -Encoding Ascii
 
-if ($someComplexCondition)
-{
-    Write-Error -Message "Some complex condition"
-}
-else
-{
-    Write-Error -Message "Other complex condition"
-}
+@'
+esac
+
+export GIT_AUTHOR_NAME="$n"
+export GIT_AUTHOR_EMAIL="$m"
+export GIT_COMMITTER_NAME="$n"
+export GIT_COMMITTER_EMAIL="$m"
+
+'@ | Add-Content -Path MyScript.sh -Encoding Ascii
